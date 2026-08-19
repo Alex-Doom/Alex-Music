e0a5610b0b45ed582b61e1ae83ce7885d580912f
Alex <Alex Chelnok>
2026-08-19 22:25:06 +0300

    Clean old_branch: keep only NewLevel

--- New_Level/Ver0/AlexMusic/build/MSYS2_MinGW-Debug/CMakeFiles/AlexMusic.dir/MetadataCache.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/AlexMusic.dir/MetadataCache.cpp.obj: \
- C:\My_QT\CPP\Alex_Music\New_Level\Ver0\AlexMusic\MetadataCache.cpp
--- New_Level/Ver0/AlexMusic/build/MSYS2_MinGW-Debug/CMakeFiles/AlexMusic.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for AlexMusic.
-# This may be replaced when dependencies are built.
